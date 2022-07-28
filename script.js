698f4eacf3783d6f40264a058ca1e19d36a46170
pharned <harned.exchange@gmail.com>
2022-07-27 21:35:38 -0500

    copy+paste help and instructions from book

--- script.js
+++ script.js
@@ -1,10 +1,44 @@
 // Write your JavaScript code here!
 
+// make a form below
+
+
+// entered text for the pilot's name, the co-pilot's name, the fuel levels, and the mass of the cargo.
+
+
+// read or not ready w/validation
+
+<script>
+   window.addEventListener("load", function() {
+      let form = document.querySelector("form");
+      form.addEventListener("submit", function(event) {
+         let usernameInput = document.querySelector("input[name=username]");
+         let teamName = document.querySelector("input[name=team]");
+         if (usernameInput.value === "" || teamName.value === "") {
+            alert("All fields are required!");
+            // stop the form submission
+            event.preventDefault();
+         }
+      });
+   });
+</script>
+
+
+// Indicate what is good or bad about the shuttle and whether it is ready for launch by using the DOM to update the CSS.
+// Fetch some planetary JSON to update the mission destination with vital facts and figures about where the shuttle is headed
+
+
+
+event.preventDefault();
+
+// below is planets list stuff
+
 window.addEventListener("load", function() {
 
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
    let listedPlanetsResponse;
+   
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
@@ -13,4 +47,6 @@ window.addEventListener("load", function() {
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
    })
    
+   
 });
+
