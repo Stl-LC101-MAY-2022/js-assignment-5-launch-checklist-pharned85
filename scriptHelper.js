16a618991a94553defae2398c9e33910bc30f558
pharned <harned.exchange@gmail.com>
2022-07-30 18:39:41 -0500

    rough drafting on scripthelper plus added planetjson link

--- scriptHelper.js
+++ scriptHelper.js
@@ -8,26 +8,38 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
    missiontarget.innerHTML = `
             <h2>Mission Destination</h2>
                 <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
-                    <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
-                </ol>
-                <img src="">
-
+                <li>Name: ${name}</li>
+                <li>Diameter: ${diameter} </li>
+                <li>Star: ${star}</li>
+                <li>Distance from Earth: ${distance}</li>
+                <li>Number of Moons: ${moons}</li>
+            </ol>
+            <img src=${imageUrl}>
 }
 
+
+//use NaN for this via instructions in assignment
 function validateInput(testInput) {
    
 }
 
+//write CSS stuff here for colors and such when the input/parameters are wrong
+
+
+//just copypasta here to remind me to fix later
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+   let pilotStatus = document.getElementById("pilotStatus");
+   let pilotStatus = document.getElementById("pilotStatus");
+   let pilotStatus = document.getElementById("pilotStatus");
+   let pilotStatus = document.getElementById("pilotStatus");
+   let pilotStatus = document.getElementById("pilotStatus");
+   let pilotStatus = document.getElementById("pilotStatus");
 }
 
+
+//return here
 async function myFetch() {
-    let planetsReturned;
+    let planetsReturned = await fetch('https://handlers.education.launchcode.org/static/planets.json');
 
     planetsReturned = await fetch().then( function(response) {
         });
@@ -36,7 +48,13 @@ async function myFetch() {
 }
 
 function pickPlanet(planets) {
+    let randomIndex = Math.floor(Math.random() * planets.length);
+    let currentPlanet = planets[randomIndex];
+    return currentPlanet;
 }
+
+
+// do not touch
 
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
