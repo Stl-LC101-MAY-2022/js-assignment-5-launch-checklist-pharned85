9622a6cb410133d0eb9456d43629e0db6adcb1bb
pharned <harned.exchange@gmail.com>
2022-08-01 02:38:40 -0500

    https://www.youtube.com/watch?v=nhagVcjOPZE

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 // make a form below
 
+// entered text for the pilot's name, the co-pilot's name, the fuel levels, and the mass of the cargo.
 window.addEventListener("load", function() {
+    const document = window.document
+    const pilot = document.querySelector("input[name=pilotName]").value.trim();
+    const copilot = document.querySelector("input[name=copilotName]").value.trim();
+    const fuelLevel = document.querySelector("input[name=fuelLevel]").value.trim();
+    const cargoLevel = document.querySelector("input[name=cargoMass]").value.trim();
+    const list = document.getElementById("faultyItems");
+
+    list.style.visibility = "hidden";   
+
+// read or not ready w/validation VVVVVVV
+// Indicate what is good or bad about the shuttle and whether it is ready for launch by using the DOM to update the CSS.
+ 
+let form = document.querySelector("form"); 
+
+formSubmission(document, list, pilot.value, copilot.value, fuelLevel.value, cargoLevel.value);
+
+
+// Fetch some planetary JSON to update the mission destination with vital facts and figures about where the shuttle is headed
     let listedPlanets;
     // Set listedPlanetsResponse equal to the value returned by calling myFetch()
     let listedPlanetsResponse = myFetch();
@@ -10,55 +29,15 @@ window.addEventListener("load", function() {
         console.log(listedPlanets);
     }).then(function () {
         console.log(listedPlanets);
-        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+        // Below this comment call the appropriate helper functions to pick a planet fRom the list of planets and add that information to your destination.
     })
     let pickedPlanet = pickPlanet(listedPlanets);
  
          addDestinationInfo(window.document, pickedPlanet.name, pickedPlanet.diameter, pickedPlanet.star, pickedPlanet.distance, pickedPlanet.moons, pickedPlanet.image);
     
- })
 
 
+});
 
 
-// entered text for the pilot's name, the co-pilot's name, the fuel levels, and the mass of the cargo.
-window.addEventListener("load", function() {
-    const document = window.document
-    const pilot = document.querySelector("input[name=pilotName]");
-    const copilot = document.querySelector("input[name=copilot]");
-    const fuelLevel = document.querySelector("input[name=fuelLevel]");
-    const cargoLevel = document.querySelector("input[name=cargoMass]");
-    const list = document.getElementById("faultyItems"); 
-
-    list.style.visibility = "hidden";   
-
-// read or not ready w/validation VVVVVVV
-// Indicate what is good or bad about the shuttle and whether it is ready for launch by using the DOM to update the CSS.
-  
-   let form = document.querySelector("form"); 
-    form.addEventListener("submit", function(event) { 
-        
-        formSubmission(document, list, pilot.value, copilot.value, fuelLevel.value, cargoLevel.value);
-        
-        if (validateInput(pilot.value) == "" || validateInput(copilot.value) == "" || validateInput(fuelLevel.value) == "" || validateInput(cargoLevel.value) == "") {
-            list.style.visibility = "hidden"; 
-            alert("Fill out every field, nerd!");
-            event.preventDefault();
-        } 
-        if (validateInput(pilot.value) == "Is a Number" || validateInput(copilot.value) == "Is a Number") {
-            list.style.visibility = "hidden"; 
-            alert("Names are spelled with letters.");
-            event.preventDefault();
-        }
-
-        if (validateInput(fuelLevel.value) == "Not a Number" || validateInput(cargoLevel.value) == "Not a Number") {
-            list.style.visibility = "hidden"; 
-            alert("Fuel Level and Cargo Mass are numbers only.");
-            event.preventDefault();
-        }
-        event.preventDefault();
-    }) 
-})
-
-
-// Fetch some planetary JSON to update the mission destination with vital facts and figures about where the shuttle is headed
+ 
